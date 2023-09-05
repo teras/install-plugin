e52fffd8e1ad41f2c1667c34212767d7478ae171
Panayotis Katsaloulis <panayotis@panayotis.com>
2023-09-05 12:12:44 +0300

    be able to publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     `java-gradle-plugin`
     `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
 }
 
 group = "com.panayotis.gradle"
@@ -21,4 +23,44 @@ publishing {
             url = uri(layout.buildDirectory.dir("maven-repo"))
         }
     }
+}
+
+nexusPublishing {
+    repositories.sonatype()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set("Gradle native installer")
+                description.set("Gradle native installer of binary files")
+                url.set("https://github.com/teras/")
+                licenses {
+                    license {
+                        name.set("GNU Lesser General Public License, Version 2")
+                        url.set("https://www.gnu.org/licenses/lgpl-2.0.html")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Panayotis Katsaloulis")
+                        email.set("panayotis@panayotis.com")
+                        organizationUrl.set("https://www.panayotis.com")
+                    }
+                }
+//                scm {
+//                    connection.set("<<SCM Connection URL>>")
+//                    developerConnection.set("<<SCM Dev Connection URL>>")
+//                    url.set("<<Source URL>>")
+//                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
